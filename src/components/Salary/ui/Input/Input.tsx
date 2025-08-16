410e21d991f0ebdb6a6337b50f483a25fb1bd266
mikromajor <robocza.sowa@yandex.com>
2025-08-16 12:45:15 +0200

    add button changeByOne

--- src/components/Salary/ui/Input/Input.tsx
+++ src/components/Salary/ui/Input/Input.tsx
@@ -15,24 +15,42 @@ export const Input = ({ payloadsKey }: InputProps) => {
 
   const { getSalary, changeSalaryDate } = salaryActions;
 
-  const handlerInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const val = Number(e.currentTarget.value);
-
+  const handlerInputChange = (val: number) => {
     if (payloadsKey === "month" || payloadsKey === "year") {
       dispatch(changeSalaryDate({ [payloadsKey]: val }));
     } else {
       dispatch(getSalary({ [payloadsKey]: val }));
     }
   };
+  const changeByOne = (val: number) => {
+    if (payloadsKey === "month" || payloadsKey === "year") {
+      dispatch(changeSalaryDate({ [payloadsKey]: salaryReducer[payloadsKey] + val }));
+    } else {
+      dispatch(getSalary({ [payloadsKey]: salaryReducer[payloadsKey] + val }));
+    }
+  };
 
   return (
-    <input
-      className={`salary__input salary__input--${currentTheme}`}
-      id={payloadsKey}
-      type='number'
-      autoComplete='off'
-      onChange={handlerInputChange}
-      value={String(salaryReducer[payloadsKey])}
-    />
+    <>
+      <button className='salary__change-for-one' onClick={() => changeByOne(-1)}>
+        -1
+      </button>
+      <input
+        className={`salary__input salary__input--${currentTheme}`}
+        id={payloadsKey}
+        type='number'
+        autoComplete='off'
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+          const val = Number(e.currentTarget.value);
+
+          handlerInputChange(val);
+        }}
+        value={String(salaryReducer[payloadsKey])}
+      />
+
+      <button className='salary__change-by-one' onClick={() => changeByOne(1)}>
+        +1
+      </button>
+    </>
   );
 };
