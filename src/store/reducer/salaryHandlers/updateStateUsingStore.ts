58e72fc51296a265665372bd1f9cec7fe001b510
mikromajor <robocza.sowa@yandex.com>
2025-08-24 22:52:06 +0200

    fix the read salary values

--- src/store/reducer/salaryHandlers/updateStateUsingStore.ts
+++ src/store/reducer/salaryHandlers/updateStateUsingStore.ts
@@ -6,9 +6,9 @@ export const updateStateUsingStore = (state: SalaryInit) => {
   if (typeof window === "undefined") {
     return undefined;
   }
-
-  const { weekends, weekdays } = amountWeekendsAndWeekdays(state.year, state.month);
-  const dateKey = getKey(state.year, state.month);
+  const { year, month } = state;
+  const { weekends, weekdays } = amountWeekendsAndWeekdays(year, month);
+  const dateKey = getKey(year, month);
 
   try {
     const item = window.localStorage.getItem(dateKey);
@@ -19,9 +19,15 @@ export const updateStateUsingStore = (state: SalaryInit) => {
           ...SALARY_INIT,
           workDays: weekdays,
           weekendDays: weekends,
-          year: state.year,
-          month: state.month,
+          year: year,
+          month: month,
         };
+    // Check values and set them to 0 if saved value is NaN
+    Object.keys(update).forEach((key) => {
+      if (isNaN(update[key as keyof SalaryInit] as number)) {
+        update[key as keyof SalaryInit] = 0;
+      }
+    });
     Object.assign(state, update);
   } catch (e) {
     console.log("Error caught in updateStateUsingStore ->", e);
